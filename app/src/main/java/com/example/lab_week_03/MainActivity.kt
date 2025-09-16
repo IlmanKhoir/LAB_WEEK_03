cfc17218a93ee3f347df104e290a5d3e6fad7195
IlmanKhoir <muhammadfadhililmankhoir@gmail.com>
2025-09-16 21:52:04 +0700

    Commit No.5 – add back button & extend list

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -1,25 +1,43 @@
 package com.example.lab_week_03
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
-//import androidx.fragment.app.FragmentContainerView
-//import androidx.fragment.app.ListFragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+interface CoffeeListener {
+    fun onSelected(id: Int)
+    fun onBackFromDetail()
+}
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), CoffeeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.
-        fragment_container)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.
-            systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
-                systemBars.bottom)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private fun showDetail() {
+        findViewById<View>(R.id.fragment_detail).visibility = View.VISIBLE
+        findViewById<View>(R.id.divider).visibility = View.VISIBLE
+    }
+    private fun showListOnly() {
+        findViewById<View>(R.id.fragment_detail).visibility = View.GONE
+        findViewById<View>(R.id.divider).visibility = View.GONE
+    }
+    override fun onSelected(id: Int) {
+        val detailFragment =
+                supportFragmentManager.findFragmentById(R.id.fragment_detail) as DetailFragment
+        showDetail()
+        detailFragment.setCoffeeData(id)
+    }
+    override fun onBackFromDetail() {
+        showListOnly()
     }
 }
